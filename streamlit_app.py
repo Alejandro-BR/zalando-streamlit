5219fe924f7991391372c3638ca6128cd201f1ef
Alejandro-BR <barrionuevorosadoalejandro@gmail.com>
2026-01-19 09:13:32 +0100

    App

---
+++ streamlit_app.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from tensorflow import keras
+from PIL import Image, ImageOps
+
+model = keras.models.load_model("/models/zalando.keras")
+
+st.title("Ialando - Clasificación de prendas")
+
+uploaded_files = st.file_uploader(
+    "Sube una imagen de la prenda que quieres clasificar", accept_multiple_files="directory", type=["jpg", "png", "jpeg"]
+)
+
+if uploaded_file is not None:
+    # Carga y procesa la imagen
+    image = Image.Open(uploaded_files).convert("L") # Escala de grises
+    image = ImageOps.invert(image) # Invierte la imagen
+    image = image.resize((28, 28)) # Redimensiona
+    image = ImageOps.autocontrast(image)
+    st.image(image, caption="Imagen procesada")
+
+    # Convierte la imagen en un array normalizado
+    img_array = (np.array(image, dtype=np.float32) / 255.0).flatten()
+    # img_array = img_array.reshape(1, 28, 28, 1)
+
+    class_name = [
+        "T-shirt/top",
+        "Trouser",
+        "Pullover",
+        "Dress",
+        "Coat",
+        "Shirt",
+        "Sneaker",
+        "Bag",
+        "Ankle boot"
+    ]
+
+    # Prediccion
+    prediction = model.predict(img_array)
+    predicted_class = class_name[np.argmax(prediction)]
+
+    st.write(f"Vector de prediccion: ${prediction}")
+    st.write(f"La prenda es: ${predicted_class}")
+
